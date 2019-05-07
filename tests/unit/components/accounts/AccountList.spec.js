537320751d13f55d16e4ca06ccafc99bcd504afc
Angel <angel4441916@gmail.com>
2019-05-07 13:41:10 -0400

    render different color for each account unit test

--- tests/unit/components/accounts/AccountList.spec.js
+++ tests/unit/components/accounts/AccountList.spec.js
@@ -1,11 +1,13 @@
 import { expect } from 'chai'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import AccountList from '@/components/accounts/AccountList.vue'
+import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import store from '@/store.js'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
+localVue.use(VueRouter)
 
 describe('AccountList.vue', () => {
   let wrapper
@@ -21,12 +23,23 @@ describe('AccountList.vue', () => {
     wrapper.vm.$store.dispatch('addAccount', account)
   })
 
-  it('renders a new account', () => {
+  it('renders a button for a new account', () => {
     expect(wrapper.find('.account-button').exists()).to.equal(true)
   })
   it('select an account when button clicked', () => {
     wrapper.find('.account-button').trigger('click')
 
     expect(wrapper.vm.$store.state.actualAccount).to.deep.equal(account)
+  })
+  it('render different colors for each account', () => {
+    for (let i = 0; i < 4; i++) {
+      wrapper.vm.$store.dispatch('addAccount', { name: 'Color account - ' + i, income: [], expenses: [] })
+    }
+
+    expect(wrapper.find('.btn-success').exists()).to.equal(true)
+    expect(wrapper.find('.btn-primary').exists()).to.equal(true)
+    expect(wrapper.find('.btn-danger').exists()).to.equal(true)
+    expect(wrapper.find('.btn-warning').exists()).to.equal(true)
+    expect(wrapper.find('.btn-danger').exists()).to.equal(true)
   })
 })
