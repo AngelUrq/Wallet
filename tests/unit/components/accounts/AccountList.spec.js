a9330f0a3353148ae11a18a356049d5c56e927d9
Angel <angel4441916@gmail.com>
2019-05-07 12:59:14 -0400

    renders a new account unit test

---
+++ tests/unit/components/accounts/AccountList.spec.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import AccountList from '@/components/accounts/AccountList.vue'
+import Vuex from 'vuex'
+import store from '@/store.js'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('AccountList.vue', () => {
+  let wrapper
+
+  beforeEach(function () {
+    wrapper = shallowMount(AccountList, { store, localVue })
+  })
+
+  it('renders a new account', () => {
+    const ACCOUNT_NAME = 'Test account'
+
+    let account = { name: ACCOUNT_NAME, income: [], expenses: [] }
+
+    wrapper.vm.$store.state.accounts.push(account)
+
+    expect(wrapper.find('.account-button').exists()).to.equal(true)
+  })
+})
