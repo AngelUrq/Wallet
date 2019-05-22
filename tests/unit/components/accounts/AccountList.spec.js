89ad5946635304d5542efbe78bf814b5036ab8d2
Angel <angel4441916@gmail.com>
2019-05-21 20:20:40 -0400

    render, methods and events unit tests for account manager

--- tests/unit/components/accounts/AccountList.spec.js
+++ tests/unit/components/accounts/AccountList.spec.js
@@ -3,7 +3,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import AccountList from '@/components/accounts/AccountList.vue'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import store from '@/store.js'
+import TestUtils from '../../../TestUtil.js'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -13,35 +13,66 @@ describe('AccountList.vue', () => {
   let wrapper
   let account
 
-  before(function() {
-    wrapper = shallowMount(AccountList, { store, localVue })
+  describe('render', () => {
+    beforeEach(function() {
+      const store = TestUtils.getStore()
+      wrapper = shallowMount(AccountList, { store, localVue })
 
+      const ACCOUNT_NAME = 'Test account'
+
+      account = { name: ACCOUNT_NAME, income: [], expenses: [] }
+
+      wrapper.vm.$store.dispatch('addAccount', account)
+    })
+
+    it('show a button for a new account', () => {
+      expect(wrapper.find('.account-button').exists()).to.equal(true)
+    })
+
+    it('show different colors for each account', () => {
+      for (let i = 0; i < 4; i++) {
+        wrapper.vm.$store.dispatch('addAccount', { name: 'Color account - ' + i, income: [], expenses: [] })
+      }
+
+      const LIST_OF_CLASSES = ['.btn-success', '.btn-primary', '.btn-danger', '.btn-warning', '.btn-danger']
+
+      for (const BUTTON_CLASS of LIST_OF_CLASSES) {
+        expect(wrapper.find(BUTTON_CLASS).exists()).to.equal(true)
+      }
+    })
+  })
+
+  describe('events', () => {
+    beforeEach(function() {
+      const store = TestUtils.getStore()
+      wrapper = shallowMount(AccountList, { store, localVue })
+
+      const ACCOUNT_NAME = 'Test account'
+
+      wrapper.vm.$store.dispatch('addAccount', ACCOUNT_NAME)
+    })
+
+    it('when button clicked select an account', () => {
+      wrapper.find('.account-button').trigger('click')
+
+      expect(wrapper.vm.$store.state.actualAccount).to.deep.equal(account)
+    })
+  })
+
+  describe('functions', () => {
     const ACCOUNT_NAME = 'Test account'
 
-    account = { name: ACCOUNT_NAME, income: [], expenses: [] }
+    beforeEach(function() {
+      const store = TestUtils.getStore()
+      wrapper = shallowMount(AccountList, { store, localVue })
 
-    wrapper.vm.$store.dispatch('addAccount', account)
-  })
+      wrapper.vm.$store.dispatch('addAccount', ACCOUNT_NAME)
+    })
 
-  it('renders a button for a new account', () => {
-    expect(wrapper.find('.account-button').exists()).to.equal(true)
-  })
+    it('select an account method', () => {
+      wrapper.vm.chooseAccount({ name: ACCOUNT_NAME, income: [], expenses: [] })
 
-  it('select an account when button clicked', () => {
-    wrapper.find('.account-button').trigger('click')
-
-    expect(wrapper.vm.$store.state.actualAccount).to.deep.equal(account)
-  })
-
-  it('render different colors for each account', () => {
-    for (let i = 0; i < 4; i++) {
-      wrapper.vm.$store.dispatch('addAccount', { name: 'Color account - ' + i, income: [], expenses: [] })
-    }
-
-    const LIST_OF_CLASSES = ['.btn-success', '.btn-primary', '.btn-danger', '.btn-warning', '.btn-danger']
-
-    for (const BUTTON_CLASS of LIST_OF_CLASSES) {
-      expect(wrapper.find(BUTTON_CLASS).exists()).to.equal(true)
-    }
+      expect(wrapper.vm.$store.state.actualAccount).to.deep.equal(account)
+    })
   })
 })
